2c86d53b0e87e0a5fed107997f0fc0bb2b98d913
raquelgarciat <raquelgarciat@gmail.com>
2023-05-31 10:11:35 +0200

    add JS amount change

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,9 @@
 //constantes
 const playbtn = document.querySelector('.js-playbtn');
 const sel = document.querySelector('.js-select');
-const amount = document.querySelector('.js-amount');
-const msg = document.querySelector('.js-msg')
+const resultAmount = document.querySelector('.js-result-amount');
+const msg = document.querySelector('.js-msg');
+const bet = document.querySelector('.js-bet');
 
 //funciones
 function getRandomNumber(max) {
@@ -12,16 +13,20 @@ function getRandomNumber(max) {
 }
 
 function compareNumber() {
-    const randomNum = getRandomNumber(6);
-    const selValue = sel.value;
-    console.log(randomNum);
-    console.log(selValue);
-    if (selValue === randomNum) {
-        msg.innerHTML = 'Has ganado el doble de lo apostado!! :D';
-    }
-    else {
-        msg.innerHTML = 'Has perdido lo apostado... D:';
-    }
+  const randomNum = getRandomNumber(6);
+  const selValue = sel.value;
+  const betAmount = bet.value;
+  let startingAmount = 50
+  console.log(randomNum);
+  if (parseInt(selValue) === parseInt(randomNum)) {
+    msg.innerHTML = 'Has ganado el doble de lo apostado!! :D';
+    const wonAmount = startingAmount + (betAmount * 2)
+    resultAmount.innerHTML = `Saldo: ${wonAmount}$`
+  } else {
+    msg.innerHTML = 'Has perdido lo apostado... D:';
+    const lostAmount = startingAmount - betAmount;
+    resultAmount.innerHTML = `Saldo: ${lostAmount}$`;
+  }
 }
 
 function handlerBtnClick() {
